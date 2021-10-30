0b30931f8440e8e001ed9d7dfe7597f4eaeca17c
vadym685 <Dfl1v4466>
2021-10-30 17:38:04 +0300

    update comment

--- src/main/java/ua/edu/sumdu/j2se/kushnir/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/kushnir/tasks/Task.java
@@ -31,17 +31,17 @@ public class Task {
         this.repeated = true;
     }
 
-    // гетер для доступу до назви задачі
+    // Метод для доступу до назви задачі
     public String getTitle() {
         return title;
     }
 
-    // сетер для зміни назви задачі
+    // Метод для зміни назви задачі
     public void setTitle(String title) {
         this.title = title;
     }
 
-    // гетер для доступу до часу виконання задачі, якщо таск повторюваний повернеться початок проміжку часу виконання задачі, в інших випадках  час вивиконання
+    // Метод для доступу до часу виконання задачі, якщо таск повторюваний повернеться початок проміжку часу виконання задачі, в інших випадках  час вивиконання
     public int getTime() {
         if (repeated) {
             return start;
@@ -50,7 +50,7 @@ public class Task {
         }
     }
 
-    // сетер для зміни часу виконання задачі, якщо таск повторюваний він буде перетворений на неповторюваний
+    // Метод для зміни часу виконання задачі, якщо таск повторюваний він буде перетворений на неповторюваний
     public void setTime(int time) {
         if (this.repeated) {
             this.repeated = false;
@@ -61,7 +61,7 @@ public class Task {
         this.time = time;
     }
 
-    // сетер для зміни часу виконання задачі, якщо таск неповторюваний він буде перетворений на повторюваний
+    // Метод для зміни часу виконання задачі, якщо таск неповторюваний він буде перетворений на повторюваний
     public void setTime(int start, int end, int interval) {
         if (!repeated) {
             this.repeated = true;
@@ -71,7 +71,8 @@ public class Task {
         this.end = end;
         this.interval = interval;
     }
-    // гетер для доступу до проміжку виконання задачі, якщо таск неповторюваний повернеться час вивиконання, в інших випадках початок проміжку часу виконання задачі
+
+    // Метод для доступу до проміжку виконання задачі, якщо таск неповторюваний повернеться час вивиконання, в інших випадках початок проміжку часу виконання задачі
     public int getStartTime() {
         if (!repeated) {
             return time;
@@ -80,10 +81,12 @@ public class Task {
         }
     }
 
+    // Метод для зміни проміжку часу виконання задачі
     public void setStartTime(int start) {
         this.start = start;
     }
 
+    // Метод для доступу до проміжку виконання задачі, якщо таск неповторюваний повернеться час вивиконання, в інших випадках кінецб проміжку часу виконання задачі
     public int getEndTime() {
         if (!repeated) {
             return time;
@@ -92,34 +95,42 @@ public class Task {
         }
     }
 
+    // Метод для зміни проміжку часу виконання задачі
     public void setEndTime(int end) {
         this.end = end;
     }
 
+    // Метод для доступу до інтервалу з яким повторюється задача
     public int getInterval() {
         return interval;
     }
 
+    // Метод для зміни інтервалу з яким повторюється задача
     public void setInterval(int interval) {
         this.interval = interval;
     }
 
+    // Метод для доступу до поля яке вказує на те чи задача активна/не активна
     public boolean isActive() {
         return active;
     }
 
+    // Метод для зміни поля яке вказує на те чи задача активна/не активна
     public void setActive(boolean active) {
         this.active = active;
     }
 
+    // Метод для доступу до поля яке вказує на те чи задача повторювана/не повторювана
     public boolean isRepeated() {
         return repeated;
     }
 
+    // Метод для зміни поля яке вказує на те чи задача повторювана/не повторювана
     public void setRepeated(boolean repeated) {
         this.repeated = repeated;
     }
 
+    // Метод для доступу до інтервалу з яким повторюється задача, якщо задача не повторювана, то повертається 0, в інших випадках інтервал
     public int getRepeatInterval() {
         if (repeated) {
             return interval;
@@ -128,6 +139,7 @@ public class Task {
         }
     }
 
+    // Метод для пошуку наступного часу виконання задачі
     public int nextTimeAfter(int current) {
         if (!active) {
             return -1;
