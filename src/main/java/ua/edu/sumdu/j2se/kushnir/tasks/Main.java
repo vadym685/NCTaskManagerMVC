fcf95c3cc8b4dec75696d4a4b5c93137b0214884
vadym685 <Dfl1v4466>
2021-11-06 16:29:34 +0200

    add class ArrayTaskList

--- src/main/java/ua/edu/sumdu/j2se/kushnir/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/kushnir/tasks/Main.java
@@ -5,10 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 //		System.out.println("Hello");
 		Task task = new Task("Shopping",11,14,2);
+		Task task1 = new Task("Shopping",11,14,2);
+		Task task2 = new Task("Shopping",11,14,2);
 		task.setActive(true);
 
 		int nextTimeAfter = task.nextTimeAfter(11);
 		System.out.println(nextTimeAfter);
+
+		ArrayTaskList arrayTaskList = new ArrayTaskList();
+
+		arrayTaskList.add(task);
+		arrayTaskList.add(task1);
+		arrayTaskList.add(task2);
+		arrayTaskList.add(null);
 
 	}
 }
